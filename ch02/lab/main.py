9c050bcd98c52ac7af9aa672ec5283bf93163fc7
cvarvaro <christianvarv@gmail.com>
2023-02-20 17:48:05 -0500

    update

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,52 @@
+import random
+import turtle
+import pygame
+import math
 
 #Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+turtle1 = turtle.Turtle()
+turtle2 = turtle.Turtle()
+
+turtle1.penup()
+turtle2.penup()
+
+turtle1.goto(100,20)
+turtle2.goto(100,-20)
+
+turtle1.goto(random.randrange(1,101),20)
+turtle2.goto(random.randrange(1,101),20)
+
+pygame.time.wait(100)
+
+turtle1.goto(100,20)
+turtle2.goto(100,-20)
+turtle1.pendown()
+turtle2.pendown()
+
+for i in range(10):
+    turtle1.goto(turtle.xcor()+ random.randrange(1, 11),20)
+    turtle2.goto(turtle.xcor()+ random.randrange(1, 11),20)
 
 
 #Part B
+pygame.init()
+window = pygame.display.set_mode()
+
+def shape(window, number_sides):
+    coordinates = []
+    number_sides = number_sides
+    side_length = 100
+    offset = 100
+    for i in range(number_sides):
+        angle = (2.0 * math.pi)/number_sides
+        x_side = side_length * math.cos(angle * i) + offset
+        y_side = side_length * math.sin(angle * i) + offset
+        coordinates.append([x_side, y_side])
+    window.fill('black')
+    pygame.draw.polygon(window, "white", coordinates)
+    pygame.display.flip()
+    pygame.time.delay(1000)
+
+for num_sides in [3,4,6,20,100,360]:
+    shape(window, num_sides)
+
