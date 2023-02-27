b48596fa3436d0affb3050b70d3c9e7ad9359dc0
cvarvaro <christianvarv@gmail.com>
2023-02-27 13:53:15 -0500

    update

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,6 +1,6 @@
 import random
 
-randomNumber = random.randrange(1,10)
+randomNumber = random.randrange(1,11)
 
 for i in range(3):
     guess = int(input("Guess a number between 1 and 10: "))
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,26 +1,48 @@
-import turtle #1. import modules
 import random
+import pygame
+import math
 
-#Part A
-window = turtle.Screen() # 2.  Create a screen
-window.bgcolor('lightblue')
+# Part A
+pygame.init()
 
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
+while 1:
+  pygame.event.pump()
+  
+  screen = pygame.display.set_mode((600,600))
+  screen_size_variable = pygame.display.get_window_size()
+  center = (screen_size_variable[0] // 2, screen_size_variable[1] // 2)
+  
+  vertical_line_start = (300,600)
+  vertical_line_end = (300,0)
+  horizontal_line_start = (0,300)
+  horizontal_line_end = (600,300)
 
-michelangelo.up() # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+  screen.fill("yellow")
+  pygame.draw.circle(screen, "light blue", center, screen_size_variable[1] // 2)
+  pygame.draw.circle(screen, "black", center, screen_size_variable[1] // 2, 5)
+  pygame.draw.line(screen, "black", vertical_line_start, vertical_line_end)
+  pygame.draw.line(screen, "black", horizontal_line_start, horizontal_line_end)
+  break
 
-## 5. Your PART A code goes here
+# Part B
 
+radius = 10
+for i in range(10):
+    x_coord = random.randrange(0, 600)
+    y_coord = random.randrange(0, 600)
+    coordinate = (x_coord, y_coord)
+    distance_from_center = math.hypot(center[0] - x_coord, center[1] - y_coord) #the distance formula
+    is_in_circle = distance_from_center <= 600/2 #screen width
+   
+    if is_in_circle:
+        pygame.draw.circle(screen, "red", coordinate, radius)
+        pygame.display.flip()
+        pygame.time.wait(1000)
+    else:
+        pygame.draw.circle(screen, "black", coordinate, radius)
+        pygame.display.flip()
+        pygame.time.wait(1000)
 
-# PART B - complete part B here
+    #2 seconds pause before ending the program. Increase if needed.
 
-
-window.exitonclick()
+  
