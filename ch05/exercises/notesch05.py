21b6e3bd428795a7b6b9edb85913097a55223518
cvarvaro <christianvarv@gmail.com>
2023-03-13 18:06:33 -0400

    update

--- ch05/exercises/notesch05.py
+++ ch05/exercises/notesch05.py
@@ -2,3 +2,77 @@ import random
 
 # Functions should have on responsibility
 # Avoid making functions responsible for input 
+
+# def foo(x, y, z):
+#     print(x, y, z)
+
+# foo(1,2,3)
+# foo(2,1,3)
+# #foo(3, 2) # ERROR - Not enough parameters 
+
+# def bar(x=0,y=2,z=3):
+#     print(x, y, z) 
+
+# bar(1,2)
+
+# global_var = 5
+# def foo():
+#     local_var = 5 # local sope
+
+# Local variables get deleted at the end of the function
+# Parameters are local variables
+
+# return None: NoneType
+
+# def bar():
+#     x = 5
+#     return x, 6
+
+# y = bar()
+# print(y)
+
+# name collisions 
+# global variables never leave memory while the program is running
+
+# def percentage_to_letter(percent):
+#     let = "A"
+#     if 80 <= percent < 90:
+#         let = "B"
+#     elif 70 <= percent < 80:
+#         let = "C"
+#     elif 60 <= percent < 70:
+#         let = "D"
+#     elif percent < 60:
+#         let = "F"
+#     return let
+
+# def is_passing(letter):
+#     return letter.lower() in "abc"
+
+# def main():
+#     grades = [90, 80, 70, 60, 50]
+#     for grade in grades:
+#         letter = percentage_to_letter(grade)
+#         if is_passing(letter):
+#             print("you passed!")
+#         else:
+#             print("Someone messed up your grades")
+
+# main()
+
+def remove_vowels(string):
+    vowels = "aeiou"
+    vowels += vowels.upper()
+    result = ""
+    for ch in string:
+        if ch not in vowels:
+            result += ch
+    return result
+
+def main():
+    mystring = "Hello World!"
+    mystring = remove_vowels(mystring)
+    print(mystring)
+
+
+main()
---
+++ ch05/exercises/scope.py
@@ -0,0 +1,5 @@
+def mult(num1, num2):
+    accumulator = 0
+    for i in range(num2):
+        accumulator += num1
+    return accumulator
