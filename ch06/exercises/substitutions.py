101fc2e8266ea248f5e2f8d55f671298f268306a
cvarvaro <christianvarv@gmail.com>
2023-03-20 21:51:49 -0400

    update

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,4 +1,18 @@
 import json
 
-file_pointer = open("news.txt", "r")
-file_pointer.readlines())
+def main():
+    news = "news.txt"
+    json = "susb.json"
+    fptr = open(news, "r")
+    fptr2 = open(json, "r")
+    accumulator = 0
+    for ch in fptr.read():
+        if ch.isalnum():
+            accumulator += 1
+    fptr.close()
+    
+    fptr = open("news.txt", "w")
+    data = str(accumulator) + "something"
+    fptr.write()
+
+
